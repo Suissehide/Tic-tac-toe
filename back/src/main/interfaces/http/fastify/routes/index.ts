56f1a6a47d66daccb279a34631f2bf42d904e805
Leo Couffinhal <leo.couffinhal@qwetle.fr>
2026-03-05 01:04:53 +0100

    feat(back): register rooms routes

--- back/src/main/interfaces/http/fastify/routes/index.ts
+++ back/src/main/interfaces/http/fastify/routes/index.ts
@@ -3,6 +3,7 @@ import type { FastifyPluginAsyncZod } from 'fastify-type-provider-zod'
 import { authRouter } from './auth'
 import { healthcheckRouter } from './healthcheck'
 import { userRouter } from './user'
+import { roomsRouter } from './rooms'
 
 const routes: FastifyPluginAsyncZod = async (fastify) => {
   fastify.get('/', async () => {
@@ -12,6 +13,7 @@ const routes: FastifyPluginAsyncZod = async (fastify) => {
   await fastify.register(healthcheckRouter)
   await fastify.register(authRouter, { prefix: '/auth' })
   await fastify.register(userRouter, { prefix: '/user' })
+  await fastify.register(roomsRouter, { prefix: '/rooms' })
 }
 
 export { routes }
--- back/src/main/interfaces/http/fastify/routes/rooms/index.ts
+++ back/src/main/interfaces/http/fastify/routes/rooms/index.ts
@@ -1,8 +1,10 @@
 import type { FastifyPluginAsync } from 'fastify'
 import { createRoomRouter } from './create.router'
 import { joinRoomRouter } from './join.router'
+import { wsRoomRouter } from './ws.router'
 
 export const roomsRouter: FastifyPluginAsync = async (fastify) => {
   await fastify.register(createRoomRouter)
   await fastify.register(joinRoomRouter)
+  await fastify.register(wsRoomRouter)
 }
